6e0fbf6022b55876a36008caa3373c36f0ccffa2
Gabriel <gabrielalcantara2705@gmail.com>
2025-03-09 02:18:41 +0000

    Refactor system monitor script for better error handling

--- main.sh
+++ main.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-if [ "$(vmstat 1 2 | awk 'NR==4 {print 100 - $15}')" -eq 100 ]; then
-	echo "$(date) ALERT: Cpu usage too high"
+cpu_usage=$(vmstat 1 2 | awk 'NR==4 {print 100 - $15}')
+if [ -z "$cpu_usage" ]; then
+	echo "$(date) ERROR: failed to fetch CPU usage"	
+else 
+	if [ "$cpu_usage" -eq 100 ]; then
+		echo "$(date) ALERT: Cpu usage too high"
+	fi
 fi
 
-if [ "$(free -m | awk '/Mem:/ {print $4}')" -lt 500 ]; then
-	echo "$(date) ALERT: Low memory!"
+free_memory=$(free -m | awk '/Mem:/ {print $4}')
+if [ -z "$free_memory" ]; then
+	
+	echo "$(date) ERROR: failed to fetch memory information"
+
+else
+
+	if [ "$free_memory" -lt 500 ]; then
+		echo "$(date) ALERT: Low memory!"
+	fi
+
 fi
 
-if [ "$(df -B1 | awk 'NR==3 {print $4}')" -lt 524288000 ]; then
-	echo "$(date) ALERT: Low available space on disk!"
+disk_space=$(df -B1 | awk 'NR==3 {print $4}')
+if [ -z "$disk_space" ]; then
+
+	echo "$(date) ERROR: failed to fetch disk space information"
+
+else
+
+	if [ "$disk_space" -lt 524288000 ]; then
+		echo "$(date) ALERT: Low available space on disk!"
+	fi
+
 fi
+
+echo "The system has been up for: $(uptime | awk -F'up' '{print $2}' | awk -F',' '{print $1}' | tr -d ' ')"
